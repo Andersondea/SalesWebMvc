d46c46ec684bebd138888089cf6f6d16b360f23b
Anderson de Assis <deassis.anderson@hotmail.com>
2022-07-12 21:40:09 -0400

    Chave estrangeira n ao nula.

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -20,6 +20,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _contex.Department.First();
             _contex.Add(obj);
             _contex.SaveChanges();
         }
