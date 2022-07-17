dad775a18febd735730cfb4ab33f787adc70d6e2
Anderson de Assis <deassis.anderson@hotmail.com>
2022-07-17 18:47:54 -0400

    Excecao para delecao.

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -32,10 +32,17 @@ namespace SalesWebMvc.Services
         }
 
         public async Task RemoveAsync(int id)
-        {
-            var obj = await _contex.Seller.FindAsync(id);
-            _contex.Seller.Remove(obj);
-            await _contex.SaveChangesAsync();
+        {            
+            try
+            {
+                var obj = await _contex.Seller.FindAsync(id);
+                _contex.Seller.Remove(obj);
+                await _contex.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Vendedor com vendas, não pode ser excluído.");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
