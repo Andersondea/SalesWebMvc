0ec9529ac9326e1945580799fffeef0e91068e08
Anderson de Assis <deassis.anderson@hotmail.com>
2022-07-17 09:21:31 -0400

    Seller Update.

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -20,21 +21,38 @@ namespace SalesWebMvc.Services
         public List<Seller> FindAll() => _contex.Seller.ToList();
 
         public void Insert(Seller obj)
-        {           
+        {
             _contex.Add(obj);
             _contex.SaveChanges();
         }
 
-        public Seller FindById (int id)
+        public Seller FindById(int id)
         {
             return _contex.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
-        public void Remove (int id)
+        public void Remove(int id)
         {
             var obj = _contex.Seller.Find(id);
             _contex.Seller.Remove(obj);
             _contex.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_contex.Seller.Any(s => s.Id == obj.Id))
+            {
+                throw new NotFoundException("Id não encontrado.");
+            }
+            try
+            {
+                _contex.Update(obj);
+                _contex.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
