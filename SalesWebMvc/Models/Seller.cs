d8eb6a855b16dd9a72e20eac71673a8ff1b5e36c
Anderson de Assis <deassis.anderson@hotmail.com>
2022-07-17 14:16:04 -0400

    Validation.

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -21,24 +21,33 @@ namespace SalesWebMvc.Models
         }
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} é obrigatório.")]
+        [StringLength(60, MinimumLength =3, ErrorMessage ="{0} deve ser entre {2} e {1}.")]
         public string Name { get; set; }
-        
+
+
+        [Required(ErrorMessage = "{0} é obrigatório.")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Entre com um e-mail válido.")]
         public string Email { get; set; }
-        
+
+        [Required(ErrorMessage = "{0} é obrigatório.")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Range(100,5000, ErrorMessage ="{0} deve ser entre {1} e {2}.")]
         public double BaseSalary{ get; set; }
-        
+
+        [Required(ErrorMessage = "{0} é obrigatório.")]
         [Display(Name="Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-
 
         public void AddSales(SalesRecord sr) => Sales.Add(sr);
         
